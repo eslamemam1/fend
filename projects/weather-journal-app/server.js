2623cf433349d8d50d137d9d3f2c37faf6389595
eslamemam1 <eslamemam65@gmail.com>
2024-11-02 08:12:04 +0200

    css edit

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -3,7 +3,11 @@ projectData = {};
 
 // Require Express to run server and routes
 
+const express = require('express');
+
 // Start up an instance of app
+
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -12,8 +16,13 @@ app.use(bodyParser.json());
 
 // Cors for cross origin allowance
 
+const cors = require('cors');
+
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+
+const port = 8080 ;
+
